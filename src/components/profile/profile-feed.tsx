cd5e0013edb87de2e1a25df401113efb2814b924
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-10-11 17:23:21 -0300

    Refatorar configurações do ESLint e adicionar regras personalizadas

---
+++ src/components/profile/profile-feed.tsx
@@ -0,0 +1,13 @@
+import { memo } from "react";
+import { TweetItem } from "../tweet/tweet-item";
+import { tweet } from "@/data/tweet";
+
+export const ProfileFeed = memo(() => {
+  return (
+    <div>
+      <TweetItem tweet={tweet} />
+      <TweetItem tweet={tweet} />
+      <TweetItem tweet={tweet} />
+    </div>
+  );
+});
