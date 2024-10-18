90793bb9060f836a00b692c1dc22d70396ce2892
gustavo.aguiar <imartytk@hotmail.com>
2024-10-18 18:07:30 -0300

    Refactorar caminhos de importação do diretório "components" no arquivo components.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import { Logo } from "@/shared/ui/atoms/twitter/logo";
-import { redirect } from "next/navigation";
 
 export default function Page() {
-  redirect("/home");
   return (
     <div className="min-h-screen flex justify-center items-center">
       <Logo size={80} />
