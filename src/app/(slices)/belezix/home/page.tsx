4664b97605c0090791160c861d0faff6b5afa9f6
gustavo.aguiar <imartytk@hotmail.com>
2024-10-20 15:51:29 -0300

    Refatorar caminhos de importação do diretório "components" no arquivo components.json

--- src/app/(slices)/belezix/home/page.tsx
+++ src/app/(slices)/belezix/home/page.tsx
@@ -1,15 +1,15 @@
 import Header from "../_components/organisms/header";
 import { ListBooking } from "../_components/organisms/list-booking";
-import { QuickSearch } from "../_components/organisms/quick-search";
 import { SubHeader } from "../_components/organisms/subheader";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <SubHeader />
-
-      <ListBooking />
+      <div>
+        <SubHeader />
+        <ListBooking />
+      </div>
     </div>
   );
 }
