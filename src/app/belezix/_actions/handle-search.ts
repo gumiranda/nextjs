21e4573cbfcf2420667d325d84da79d15f9a07e2
gustavo.aguiar <imartytk@hotmail.com>
2024-10-21 16:57:25 -0300

    Update @types/react to version 18.3.11 Remove handleSearch action from belezix/_actions/handle-search.ts Refactor search component in belezix/_components/molecules/search.tsx

--- src/app/belezix/_actions/handle-search.ts
+++
@@ -1,6 +0,0 @@
-"use server";
-import { redirect } from "next/navigation";
-
-export async function handleSearch(formData: FormData) {
-  redirect(`/belezix/owners?q=${formData.get("title")}`);
-}
--- src/app/belezix/_components/molecules/search.tsx
+++ src/app/belezix/_components/molecules/search.tsx
@@ -3,7 +3,7 @@
 import { SearchIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
-import NextForm from "next/form";
+
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import {
@@ -15,7 +15,6 @@ import {
 } from "@/shared/ui/molecules/shadcn/form";
 import { Input } from "@/shared/ui/atoms/shadcn/input";
 import { Button } from "@/shared/ui/atoms/shadcn/button";
-import { handleSearch } from "../../_actions/handle-search";
 
 const formSchema = z.object({
   title: z.string().trim().min(1, {
@@ -38,11 +37,7 @@ const Search = () => {
 
   return (
     <Form {...form}>
-      <NextForm
-        onSubmit={form.handleSubmit(handleSubmit)}
-        className="flex gap-2"
-        action={handleSearch}
-      >
+      <form onSubmit={form.handleSubmit(handleSubmit)} className="flex gap-2">
         <FormField
           control={form.control}
           name="title"
@@ -62,7 +57,7 @@ const Search = () => {
         <Button type="submit">
           <SearchIcon />
         </Button>
-      </NextForm>
+      </form>
     </Form>
   );
 };
